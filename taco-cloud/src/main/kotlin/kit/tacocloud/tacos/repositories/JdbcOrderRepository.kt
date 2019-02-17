c89820f5c7f332ffde0d83947e0ab094e0d4f985
Kitovsky <d.kitovsky@gmail.com>
2019-02-17 14:32:04 +0300

    extra repos are added

---
+++ taco-cloud/src/main/kotlin/kit/tacocloud/tacos/repositories/JdbcOrderRepository.kt
@@ -0,0 +1,45 @@
+package kit.tacocloud.tacos.repositories
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.convertValue
+import kit.tacocloud.tacos.domain.Order
+import kit.tacocloud.tacos.domain.Taco
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
+import java.util.Date
+
+class JdbcOrderRepository(
+        @Autowired private val jdbc: JdbcTemplate
+) : OrderRepository {
+    private val orderInserter = SimpleJdbcInsert(jdbc)
+            .withTableName("Taco_Order")
+            .usingGeneratedKeyColumns("id")
+
+    private val orderTacoInserter = SimpleJdbcInsert(jdbc)
+            .withTableName("Taco_Order_Tacos")
+
+    private val objectMapper = ObjectMapper()
+
+    override fun save(order: Order): Order {
+        order.placedAt = Date()
+        order.id = saveOrderDetails(order)
+        order.tacos.forEach {
+            saveTacoToOrder(it, order.id!!)
+        }
+        return order
+    }
+
+    private fun saveTacoToOrder(taco: Taco, orderId: Long) {
+        val values = HashMap<String, Any>()
+        values["tacoOrder"] = orderId
+        values["taco"] = taco.id!!
+        orderTacoInserter.execute(values)
+    }
+
+    private fun saveOrderDetails(order: Order): Long {
+        val values = objectMapper.convertValue<MutableMap<String, Any>>(order)
+        values["placedAt"] = order.placedAt
+        return orderInserter.executeAndReturnKey(values).toLong()
+    }
+}
