5ca290ee6fc9a94db887d3019d42b691c01052bf
Kitovsky <d.kitovsky@gmail.com>
2019-03-16 21:44:21 +0300

    basic LDAP config is added

--- taco-cloud/src/main/kotlin/kit/tacocloud/tacos/security/SecurityConfig.kt
+++ taco-cloud/src/main/kotlin/kit/tacocloud/tacos/security/SecurityConfig.kt
@@ -1,25 +1,25 @@
 package kit.tacocloud.tacos.security
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import javax.sql.DataSource
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig(
-        @Autowired val dataSource: DataSource
-) : WebSecurityConfigurerAdapter() {
+class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, enabled from User" +
-                        "where username=?")
-                .authoritiesByUsernameQuery("select username, authority from UserAuthorities" +
-                        "where username=?")
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .groupSearchBase("ou=groups")
+                .userSearchFilter("(uid={0})")
+                .groupSearchFilter("(member={0})")
+                .passwordCompare()
                 .passwordEncoder(BCryptPasswordEncoder(5))
+                .passwordAttribute("passcode")
+                .and()
+                .contextSource()
+                .url("ldap://tacocloud.com:389/dc=tacocloud,dc=com")
     }
 }
