4b39b96689f56a86b8534d57f2f62047751bac5b
Kitovsky <d.kitovsky@gmail.com>
2019-03-16 21:33:22 +0300

    jdbc security is added

--- taco-cloud/src/main/kotlin/kit/tacocloud/tacos/security/SecurityConfig.kt
+++ taco-cloud/src/main/kotlin/kit/tacocloud/tacos/security/SecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import javax.sql.DataSource
 
 @Configuration
@@ -13,13 +14,12 @@ class SecurityConfig(
         @Autowired val dataSource: DataSource
 ) : WebSecurityConfigurerAdapter() {
     override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password("test")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("woody")
-                .password("test2")
-                .authorities("ROLE_USER")
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from User" +
+                        "where username=?")
+                .authoritiesByUsernameQuery("select username, authority from UserAuthorities" +
+                        "where username=?")
+                .passwordEncoder(BCryptPasswordEncoder(5))
     }
 }
