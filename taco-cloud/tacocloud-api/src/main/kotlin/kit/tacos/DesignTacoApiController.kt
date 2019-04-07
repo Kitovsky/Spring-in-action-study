7f37bcad40d8e406e2c4bca5579f7fc41a4b4d3f
Kitovsky <d.kitovsky@gmail.com>
2019-04-07 18:12:45 +0300

    assembler is added

--- taco-cloud/tacocloud-api/src/main/kotlin/kit/tacos/DesignTacoApiController.kt
+++ taco-cloud/tacocloud-api/src/main/kotlin/kit/tacos/DesignTacoApiController.kt
@@ -1,14 +1,17 @@
 package kit.tacos
 
 import kit.tacos.data.TacoRepository
+import kit.tacos.domain.Ingredient
 import kit.tacos.domain.Taco
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.hateoas.Resource
+import org.springframework.hateoas.ResourceSupport
 import org.springframework.hateoas.Resources
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
 import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.Date
 
 @RestController
 @RequestMapping(path = ["/design"], produces = [APPLICATION_JSON_VALUE])
@@ -50,5 +54,26 @@ class DesignTacoApiController(
     @ResponseStatus(HttpStatus.CREATED)
     fun postTaco(@RequestBody taco: Taco): Taco {
         return tacoRepo.save(taco)
+    }
+
+    private class TacoResourceAssembler()
+        : ResourceAssemblerSupport<Taco, TacoResource>(DesignTacoApiController::class.java,
+            TacoResource::class.java) {
+
+        override fun instantiateResource(entity: Taco) = TacoResource.from(entity)
+
+        override fun toResource(entity: Taco) = createResourceWithId(entity.id, entity)
+    }
+
+    data class TacoResource(
+            val name: String,
+            val createdAt: Date?,
+            val ingredients: List<Ingredient>
+    ) : ResourceSupport() {
+        companion object {
+            fun from(taco: Taco) = TacoResource(taco.name,
+                    taco.createdAt,
+                    taco.ingredients)
+        }
     }
 }
