89930f0246ed8d6d875eecd8360f582009c64a53
Kitovsky <d.kitovsky@gmail.com>
2019-05-03 01:00:42 +0300

    partial migration gradle groovy to gradle kotlin

---
+++ taco-cloud/tacocloud-security/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    implementation(project(":tacocloud-domain"))
+    implementation(project(":tacocloud-data"))
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
