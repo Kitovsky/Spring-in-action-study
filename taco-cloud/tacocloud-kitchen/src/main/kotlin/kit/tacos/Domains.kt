9037d864692e141205bd7c4bfa58dbdd6a453d77
Kitovsky <d.kitovsky@gmail.com>
2019-06-05 00:16:33 +0300

    fixes

--- taco-cloud/tacocloud-kitchen/src/main/kotlin/kit/tacos/Domains.kt
+++ taco-cloud/tacocloud-kitchen/src/main/kotlin/kit/tacos/Domains.kt
@@ -2,9 +2,9 @@ package kit.tacos
 
 import java.util.Date
 
-data class Ingredient(
-        val name: String,
-        val type: Type
+data class Ingredient( //default values only for json deserialization
+        var name: String = "",
+        var type: Type = Type.CHEESE
 ) {
     enum class Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
